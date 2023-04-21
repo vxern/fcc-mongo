309e72a3e79fa95e64b8c745968a18471d3f583d
vxern <vxern@wordcollector.co.uk>
2023-04-21 14:01:43 +0100

    misc: Complete milestone 12.

--- myApp.js
+++ myApp.js
@@ -115,7 +115,16 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person
+    .find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((error, data) => {
+      if (error) return console.error(error);
+
+      return done(null, data);
+    });
 };
 
 /** **Well Done !!**
