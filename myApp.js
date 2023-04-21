42cca2adfd9c3c9ffcde210ee28e809396632a12
vxern <vxern@wordcollector.co.uk>
2023-04-21 13:10:22 +0100

    misc: Complete milestone 9.

--- myApp.js
+++ myApp.js
@@ -82,7 +82,16 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (error, data) => {
+      if (error) return console.error(error);
+
+      return done(null, data);
+    },
+  );
 };
 
 const removeById = (personId, done) => {
